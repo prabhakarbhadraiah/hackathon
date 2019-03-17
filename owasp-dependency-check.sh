360b7f46615836db438c091eb3bdb6cfeba13f5c
devopssecure <48332859+devopssecure@users.noreply.github.com>
2019-03-17 10:31:20 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -7,13 +7,23 @@ REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directories"
     mkdir -p "$DATA_DIRECTORY"
-    chmod -R 777 "$DATA_DIRECTORY"
+    chmod -R 664 "$DATA_DIRECTORY"
 
     mkdir -p "$REPORT_DIRECTORY"
-    chmod -R 777 "$REPORT_DIRECTORY"
+    chmod -R 664 "$REPORT_DIRECTORY"
 fi
 
 # Make sure we are using the latest version
 docker pull owasp/dependency-check
 
-docker run --rm --volume `pwd`:/src --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data --volume "$REPORT_DIRECTORY":/report  owasp/dependency-check --scan /src --format "ALL" --project "My OWASP Dependency Check Project" --out /report
+docker run --rm \
+    --volume $(pwd):/src \
+    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
+    --volume "$REPORT_DIRECTORY":/report \
+    owasp/dependency-check \
+    --scan /src \
+    --format "ALL" \
+    --project "My OWASP Dependency Check Project" \
+    --out /report
+    # Use suppression like this: (/src == $pwd)
+    # --suppression "/src/security/dependency-check-suppression.xml"
